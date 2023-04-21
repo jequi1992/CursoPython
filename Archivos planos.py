e74d9a9b68127dfc1bd79d1a080b9dd3d528fa60
Jhon Esteban Quiroga Lozano <73084524+jequi1992@users.noreply.github.com>
2023-04-21 13:47:17 +0000

    Cambios ruta

---
+++ Archivos planos.py
@@ -0,0 +1,5 @@
+print("*******************************************")
+print("PROGRAMA QUE ABRE, LEE, IMPRIME ARCHIVO TXT")
+print("*******************************************")
+archivo=open("/workspaces/CursoPython/chavo.txt")
+archivo.close()
