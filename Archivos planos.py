c86b14c1ae273445bd7690cdcfbb7cd5460f8988
Jhon Esteban Quiroga Lozano <73084524+jequi1992@users.noreply.github.com>
2023-04-21 14:11:34 +0000

    Ajustes

--- Archivos planos.py
+++ Archivos planos.py
@@ -1,6 +1,7 @@
 print("*******************************************")
 print("PROGRAMA QUE ABRE, LEE, IMPRIME ARCHIVO TXT")
 print("*******************************************")
-archivo=open("https://github.com/jequi1992/CursoPython/blob/61441141a9d897ee02ff6c9d9841e2e3e81a7402/chavo.txt")
+archivo=open("https://github.com/jequi1992/CursoPython/blob/3a4795418ca23f288f101f0070f3516c637ade1d/chavo.txt")
 archivo.close()
+
 print("HOLA")
