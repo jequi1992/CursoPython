8fe2c07690b3e18ddd263ab23628528f7dba904b
Jhon Esteban Quiroga Lozano <73084524+jequi1992@users.noreply.github.com>
2023-04-28 13:20:09 +0000

    Archivo nuevo

---
+++ Archivo.py
@@ -0,0 +1,7 @@
+print("********************************************")
+print("LISTADO VECINDAD DEL CHAVO")
+print("********************************************")
+
+with open("C:\\Users\\VIVELAB\\Downloads\\vecindadchavo.csv","r") as archivo:
+    print(archivo.read())
+
