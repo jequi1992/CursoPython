fa8aa4a439cf70addab59c4fb7fab3f530b8cecc
Jhon Esteban Quiroga Lozano <73084524+jequi1992@users.noreply.github.com>
2023-05-05 10:08:43 -0500

    Cambios Aplicados

--- Diccionario.py
+++ Diccionario.py
@@ -1,10 +1,16 @@
-print("****** CREA DICCIONARIO *******")
-dicc={"Nombre":"Homero, Bart","Edad":"25,10","Sexo":"Masculino, Masculino"}
-print(dicc)
+# print("****** CREA DICCIONARIO *******")
+# dicc={"Nombre":"Homero, Bart","Edad":"25,10","Sexo":"Masculino, Masculino"}
+# print(dicc)
 
 
-for i,k in dicc.items():
-    print(i,k)
+# for i,k in dicc.items():
+#     print(i,k)
 
+# busca = input("Ingrese Clave a Buscar")
+# print(dicc.get(busca,"CLAVE NO EXISTE"))
 
+#______________________________________________________________________________________________________________________________________________________________________________
 
+dep ={"Tolima":("Ibague", "Espinal", "Guamo"),"Cundinamarca":("Bogota", "Girardot", "Tocaima"),"Antioquia":("Medellin", "Barbosa", "Segovia")}
+dep["Santander"]={"Bucaramanga","Giron","Zapatoca"}
+print(dep)
