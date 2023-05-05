d73beb688db6a83adc93a89d0ca360a6a32dbed5
Jhon Esteban Quiroga Lozano <73084524+jequi1992@users.noreply.github.com>
2023-05-05 11:31:29 -0500

    Archivo final
    Se agregan algunos otros datos.

--- Diccionario.py
+++ Diccionario.py
@@ -11,6 +11,32 @@
 
 #______________________________________________________________________________________________________________________________________________________________________________
 
-dep ={"Tolima":("Ibague", "Espinal", "Guamo"),"Cundinamarca":("Bogota", "Girardot", "Tocaima"),"Antioquia":("Medellin", "Barbosa", "Segovia")}
-dep["Santander"]={"Bucaramanga","Giron","Zapatoca"}
-print(dep)
+# dep ={"Tolima":("Ibague", "Espinal", "Guamo"),"Cundinamarca":("Bogota", "Girardot", "Tocaima"),"Antioquia":("Medellin", "Barbosa", "Segovia")}
+# dep["Santander"]={"Bucaramanga","Giron","Zapatoca"}
+# print(dep)
+
+#_________________________________________________________________________________________________________________________
+
+# import pandas as pd
+# import numpy as np
+
+# vec = np.array([2,3,4])
+# mat = np.array([[3,6,7],[9,4,2],[7,0,1]])
+# print(vec)
+# print(mat)
+
+#________________________________________________________________________________________________________
+
+# import numpy as np
+# a = np.arange(0,8,2)
+# print(a)
+
+#_____________________________________________________________________________________________________
+
+import numpy as np
+arreglo=np.array([["Luis", "Maria", "Paco"],["Luna", "Pepe", "Luis"],["Pepa", "Lucho", "Mile"]])
+
+tam = arreglo.size
+
+print(arreglo)
+print(tam)
